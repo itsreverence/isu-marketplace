be54b4e7710bb7b61b380c34b1d1edeab4fbf7c5
itsreverence <github.shakily561@passmail.net>
2026-04-06 12:57:30 -0500

    remove unused import and create tables only if thery don't exist

--- InputController.java
+++ InputController.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.util.List;
 import java.util.Scanner;
 import java.util.UUID;
--- ListingHandler.java
+++ ListingHandler.java
@@ -18,7 +18,7 @@ public class ListingHandler extends DatabaseHandler {
         try {
             Statement statement = this.connection.createStatement();
             statement.setQueryTimeout(30);
-            statement.executeUpdate("create table listing (id string, userId string, title string, description string, price float)");
+            statement.executeUpdate("if not exists (create table listing (id string, userId string, title string, description string, price float))");
         } catch (SQLException e) {
             e.printStackTrace();
         }
